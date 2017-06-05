213c99872fa7e53932aa10c267cc071ae389c829
Arslan <aarslan1995@gmail.com>
2017-06-06 01:52:01 +0300

    3

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from socket import *
+import socket
+import struct
+import cv2
+import numpy as np
 
-HOST = "192.168.0.41" #local host
-PORT = 7000 #open port 7000 for connection
-s = socket(AF_INET, SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1) #how many connections can it receive at one time
-conn, addr = s.accept() #accept the connection
-print "Connected by: " , addr #print the address of the person connected
-while True:
-    data = conn.recv(1024) #how many bytes of data will the server receive
-    print "Received: ", repr(data)
-    reply = "Hi!" #server's reply to the client
-    conn.sendall(reply)
-conn.close()
+from cStringIO import StringIO
+
+def create_opencv_image_from_stringio(img_stream, cv2_img_flag=0):
+    img_stream.seek(0)
+    img_array = np.asarray(bytearray(img_stream.read()), dtype=np.uint8)
+    return cv2.imdecode(img_array, cv2_img_flag)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 80))
+s.listen(1000)
+
+client, addr = s.accept()
+print 'got connected from', addr
+
+buf = ''
+while len(buf)<4:
+    buf += client.recv(4-len(buf))
+size = struct.unpack('!i', buf)
+print "receiving %s bytes" % size
+
+with open('tst.jpg', 'wb+') as img:
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+        img.write(data)
+    print 'received, yay! img type:', type(img)
+    res = create_opencv_image_from_stringio(img)
+    # cv2.imshow("im", res)
+    # cv2.waitKey(0)
+
+client.close()
